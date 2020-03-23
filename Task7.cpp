c546187a8e46482d2ec9d89c750cb544870ea0e0
MitiaKorotkov <55015177+MitiaKorotkov@users.noreply.github.com>
2020-03-23 14:56:34 +0300

    Update Task7.cpp

--- Task7.cpp
+++ Task7.cpp
@@ -1,28 +1,28 @@
-//#include <iostream>
-//#include <string>
-//#include <float.h>
-//#include <iomanip>
-//#include <math.h>
-//
-//
-//using namespace std;
-//
-//void foo_f() {
-//	float f = 1;
-//	float d = 0.1;
-//	for (int i = 0; i < 50; ++i) {
-//		d /= 10;
-//		cout << setprecision(100) << fixed << d << endl;
-//	}
-//	d = 0.0000000298027;
-//	f = 1;
-//	for (;;) {
-//		cout << setprecision(100) << fixed << f << endl;
-//		f -= d;
-//	}
-//}
-//
-//int main() {
-//	foo_f();
-//	return 0;
-//}
+#include <iostream>
+#include <string>
+#include <float.h>
+#include <iomanip>
+#include <math.h>
+
+
+using namespace std;
+
+void foo_f() {
+	float f = 1;
+	float d = 0.1;
+	for (int i = 0; i < 50; ++i) {
+		d /= 10;
+		cout << setprecision(100) << fixed << d << endl;
+	}
+	d = 0.0000000298027;
+	f = 1;
+	for (;;) {
+		cout << setprecision(100) << fixed << f << endl;
+		f -= d;
+	}
+}
+
+int main() {
+	foo_f();
+	return 0;
+}
