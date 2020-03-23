ed285cb167339f84c25b490290ea82505aab967b
MitiaKorotkov <55015177+MitiaKorotkov@users.noreply.github.com>
2020-03-23 14:59:03 +0300

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -1,14 +1,14 @@
-//#include <iostream>
-//#include <string>
-//
-//using namespace std;
-//
-//
-//int main() {
-//	unsigned int x;
-//	cin >> x;
-//	for (int i = sizeof(x) * 8 - 1; i >= 0; --i) {
-//		cout << (int)((x >> i) & 1);
-//	}
-//	return 0;
-//}
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+	unsigned int x;
+	cin >> x;
+	for (int i = sizeof(x) * 8 - 1; i >= 0; --i) {
+		cout << (int)((x >> i) & 1);
+	}
+	return 0;
+}
